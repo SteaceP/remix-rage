a556233de63f320e35f7e771b2b77bbff152e1a9
SteaceP <steacy@live.ca>
2024-01-14 23:31:10 -0500

    added tailwind, updated remix, modified homepage

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction } from "@remix-run/cloudflare";
-import { cssBundleHref } from "@remix-run/css-bundle";
+// import { cssBundleHref } from "@remix-run/css-bundle";
 import {
   Links,
   LiveReload,
@@ -9,9 +9,9 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,37 @@ import type { MetaFunction } from "@remix-run/cloudflare";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Code Rage" },
+    {
+      name: "description",
+      content:
+        "Welcome to Code Rage! The website is going for a overhaul! Please be patient.",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <header className="flex justify-center items-center flex-col mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold">Welcome to Code Rage</h1>
+        <h3 className="text-lg">This website is under RE-construction</h3>
+        <p className="text-base">Please be patient.</p>
+      </header>
+      <main className="flex justify-center items-center mt-8">
+        <div className="flex justify-center items-center overflow-auto">
+          <img
+            src="/assets/images/website-rebuild.jpg"
+            alt="Code Rage Website Rebuild"
+            className="max-w-full h-auto"
+          />
+        </div>
+      </main>
+      <footer className="text-center mt-8 absolute bottom-0">
+        <div className="flex justify-center items-center">
+          <p className="mx-auto">© 2024 Code Rage. All rights reserved.</p>
+        </div>
+      </footer>
+    </>
   );
 }
